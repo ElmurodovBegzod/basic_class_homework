44910baba78bb06f2c88afe2ce68d0889560a3dc
Begzod <ebegzod477@gmail.com>
2022-09-02 11:01:09 +0500

    code06

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -3,5 +3,12 @@
 /*
 Create a "Person" class that has a name("name") and a age("age")
 */
+class Person {
+  String name = 'Begzod';
+  int age = 25;
+}
 
-void main() {}
+void main() {
+  Person x = Person();
+  print(x);
+}
