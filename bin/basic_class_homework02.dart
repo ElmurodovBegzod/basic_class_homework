03fc0a59105e75222befb119520958825b84053d
Begzod <ebegzod477@gmail.com>
2022-09-02 10:39:12 +0500

    code02

--- bin/basic_class_homework02.dart
+++ bin/basic_class_homework02.dart
@@ -3,4 +3,17 @@ Create a "Person" class.
 Create an attribute "name" in the "Person" class.
 */
 
-void main() {}
+class Person {
+  String name = 'Elmurodov';
+  Person({String name = ''}) {
+    this.name = name;
+  }
+  String getInfo() {
+    return 'Name ${this.name}';
+  }
+}
+
+void main() {
+  Person x = Person(name: 'Begzod');
+  print(x.getInfo());
+}
